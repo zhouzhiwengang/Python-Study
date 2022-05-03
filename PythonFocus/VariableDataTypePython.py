0a4c764aea922b483853330c6ad42ef0a52fb02c
zhouzhiwengang <zhouzhiwengang@163.com>
2022-05-03 22:54:55 +0800

    Python 学习笔记

---
+++ PythonFocus/VariableDataTypePython.py
@@ -0,0 +1,28 @@
+# _*_ coding : UTF-8_*_
+# 开发者 ： zhuozhiwengang
+# 开发时间 : 2022/4/28 1:15
+# 文件名称 : VariableDataTypePython
+# 开发工具 : PyCharm
+
+# Python 保留字：Python语言中已经被赋予特殊含义的单词。
+
+# Python 标识符
+"""
+标识符定义规范：
+1、由字母\下划线\_\数字组成，且第一个字符不能是数字
+2、不能使用Python 保留字
+3、区分字母大小写
+"""
+
+# Python 变量
+# Python 变量赋值,通过=
+num = 5
+print("num 变量值:", num)
+# Python 是一种动态语言，变量的类型可以随时变化
+name = "zhouzhiwengang"
+print(type(name))
+name = 16
+print(type(name))
+
+# Python 基本数据类型
+
